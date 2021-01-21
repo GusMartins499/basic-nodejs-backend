bbd67603e685dd4255923d18f6db1f734165f411
Gusmartins499 <gustavosm994@gmail.com>
2021-01-20 22:43:46 -0300

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const { v4: uuidv4, validate: uuidValidate } = require('uuid');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const projects = [];
